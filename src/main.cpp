3eeabd6b7dcdc42a4920deb8203faeca2de8f2a4
shearer12345 <shearer12345@gmail.com>
2016-04-11 13:02:13 +0100

    add zmq binaries and update premake, and sample code

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-
+#include <sstream>
+#include "zmq.hpp"
 
 #ifdef _WIN32 // compiling on windows
 #include <SDL.h>
@@ -20,11 +21,62 @@ SDL_Window *win; //pointer to the SDL_Window
 SDL_Renderer *ren; //pointer to the SDL_Renderer
 SDL_Surface *surface; //pointer to the SDL_Surface
 SDL_Texture *tex; //pointer to the SDL_Texture
+SDL_Rect texture_rect; //SDL_Rect for the texture
+
 SDL_Surface *messageSurface; //pointer to the SDL_Surface for message
 SDL_Texture *messageTexture; //pointer to the SDL_Texture for message
 SDL_Rect message_rect; //SDL_rect for the message
 
 bool done = false;
+
+// ZMQVariables - our context and socket
+bool ZMQserver = false;
+zmq::context_t this_zmq_context(1);
+
+zmq::socket_t this_zmq_publisher(this_zmq_context, ZMQ_PUB); //create publisher and subscriber always (this is dumb)
+zmq::socket_t this_zmq_subscriber(this_zmq_context, ZMQ_SUB);
+
+void handleNetwork()
+{
+	const int messageLength = 40;
+	if (ZMQserver)
+	{
+		//  Send message to all subscribers
+		zmq::message_t message(messageLength);
+		snprintf((char *)message.data(), messageLength,
+			"%06.1f %06.1f %06.1f %06.1f end", float(texture_rect.x), float(texture_rect.y), float(message_rect.x), float(message_rect.y));
+
+		std::cout << "Message sent: \"" << std::string(static_cast<char*>(message.data()), message.size()) << "\"" << std::endl;
+		this_zmq_publisher.send(message);
+	}
+	else
+	{
+		const std::string filter = "";
+		this_zmq_subscriber.setsockopt(ZMQ_SUBSCRIBE, filter.c_str(), filter.length());
+
+		zmq::message_t update;
+
+		bool gotMessage = this_zmq_subscriber.recv(&update, ZMQ_DONTWAIT);
+		if (gotMessage)
+		{
+			char* the_data = static_cast<char*>(update.data());
+
+			std::cout << "Message received: \"" << std::string(the_data) << "\"" << std::endl;
+
+			std::istringstream iss(static_cast<char*>(update.data()));
+			
+			iss >> texture_rect.x >> texture_rect.y >> message_rect.x >> message_rect.y;
+
+			std::cout << "texture x, y: " << std::to_string(texture_rect.x) << ", " << std::to_string(texture_rect.y) << std::endl;
+			std::cout << "message x, y: " << std::to_string(message_rect.x) << ", " << std::to_string(message_rect.y) << std::endl;
+		}
+		else
+		{
+			std::cout << "." ;
+		}
+	}
+
+}
 
 void handleInput()
 {
@@ -71,7 +123,17 @@ void handleInput()
 // tag::updateSimulation[]
 void updateSimulation(double simLength = 0.02) //update simulation with an amount of time to simulate for (in seconds)
 {
-  //CHANGE ME
+	handleNetwork();
+
+	if (ZMQserver)
+	{
+		texture_rect.x += 1.0f;
+		texture_rect.x = texture_rect.x % (600-texture_rect.w);
+
+
+		message_rect.y += 22.0f;
+		message_rect.y = message_rect.y % (600-message_rect.h);
+	}
 }
 
 void render()
@@ -80,7 +142,7 @@ void render()
 		SDL_RenderClear(ren);
 
 		//Draw the texture
-		SDL_RenderCopy(ren, tex, NULL, NULL);
+		SDL_RenderCopy(ren, tex, NULL, &texture_rect);
 
 		//Draw the text
 		SDL_RenderCopy(ren, messageTexture, NULL, &message_rect);
@@ -102,6 +164,23 @@ void cleanExit(int returnValue)
 // based on http://www.willusher.io/sdl2%20tutorials/2013/08/17/lesson-1-hello-world/
 int main( int argc, char* args[] )
 {
+	std::cout << "argc was: " << argc << std::endl;
+	if (argc > 1)
+	{
+		std::string s(args[1]);
+		if (s == "--server")
+		{
+			ZMQserver = true;
+			std::cout << "Running as SERVER" << std::endl;
+			std::cout << "Args[1] was: \"" << args[1] << "\"" << std::endl;
+		}
+	}
+	else
+	{
+		ZMQserver = false;
+		std::cout << "Running as CLIENT" << std::endl;
+	}
+
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
@@ -161,6 +240,24 @@ int main( int argc, char* args[] )
 	message_rect.y = 0;
 	message_rect.w = 300;
 	message_rect.h = 100;
+
+	
+
+	texture_rect.x = 100;
+	texture_rect.y = 200;
+	texture_rect.w = 300;
+	texture_rect.h = 100;
+
+
+	if (ZMQserver)
+	{
+		this_zmq_publisher.bind("tcp://*:5556");
+	}
+	else
+	{
+		std::cout << "Subscribing to server ..." << std::endl;
+		this_zmq_subscriber.connect("tcp://localhost:5556");
+	}
 
 	while (!done) //loop until done flag is set)
 	{
