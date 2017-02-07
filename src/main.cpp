9de8a8c155aa7e2afcf4d3e1f189e10de18ec0f3
John Shearer <shearer12345@gmail.com>
2017-02-07 08:51:06 +0000

    make OpenGL template (using SDL2)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-// // GLEW - OpenGL Extension Wrangler - http://glew.sourceforge.net/
-// // NOTE: include before SDL.h
-// #include <GL/glew.h>
+// GLEW - OpenGL Extension Wrangler - http://glew.sourceforge.net/
+// NOTE: include before SDL.h
+#include <GL/glew.h>
 
 // SDL - Simple DirectMedia Layer - https://www.libsdl.org/
 #include "SDL.h"
@@ -10,13 +10,13 @@
 #include "SDL_mixer.h"
 #include "SDL_ttf.h"
 
-// // - OpenGL Mathematics - https://glm.g-truc.net/
-// #define GLM_FORCE_RADIANS // force glm to use radians
-// // NOTE: must do before including GLM headers
-// // NOTE: GLSL uses radians, so will do the same, for consistency
-// #include <glm/glm.hpp>
-// #include <glm/gtc/matrix_transform.hpp>
-// #include <glm/gtc/type_ptr.hpp>
+// - OpenGL Mathematics - https://glm.g-truc.net/
+#define GLM_FORCE_RADIANS // force glm to use radians
+// NOTE: must do before including GLM headers
+// NOTE: GLSL uses radians, so will do the same, for consistency
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 int main(int argc, char *argv[]) {
   // SDL initialise
@@ -29,19 +29,10 @@ int main(int argc, char *argv[]) {
 
   // Window Creation
   SDL_Window *win = nullptr;
-  win = SDL_CreateWindow("Hello World", 100, 100, 800, 600, 0);
+  win = SDL_CreateWindow("Hello World", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
   if (win == nullptr) {
     SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
                     "SDL_CreateWindow init error: %s\n", SDL_GetError());
-    return 1;
-  }
-
-  // Renderer Creation
-  SDL_Renderer *renderer = nullptr;
-  renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-  if (renderer == nullptr) {
-    SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
-                    "SDL_CreateRenderer init error: %s\n", SDL_GetError());
     return 1;
   }
 
@@ -72,28 +63,28 @@ int main(int argc, char *argv[]) {
   }
   SDL_Log("SDL_mixer initialised OK!\n");
 
-  // Load the image
-  SDL_Surface *logoSurface = IMG_Load("assets/Sdl-logo.png");
-  if (!logoSurface) {
-    SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, "IMG_Load: %s\n", IMG_GetError());
-    return 1;
-  }
-  int width = logoSurface->w;
-  int height = logoSurface->h;
-  SDL_Log("Image width:height = %d:%d\n", width, height);
-  SDL_Texture *logoTexture =
-      SDL_CreateTextureFromSurface(renderer, logoSurface);
-  SDL_Rect logoRect{100, 100, logoSurface->w, logoSurface->h};
-
-  // Load the font
-  TTF_Font *font = TTF_OpenFont("assets/OpenSans-Bold.ttf", 90);
-  if (font == nullptr) {
+  // Set OpenGL context parameters
+  int major = 4, minor = 3;
+  SDL_Log("Asking for OpenGL %d.%d context\n", major, minor);
+  
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, major);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, minor);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_PROFILE_CORE); //use core profile
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); //ask for forward compatible 
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); //default, probably 
+  
+  // OpenGL Context Creation
+  SDL_GLContext glcontext = SDL_GL_CreateContext(win);
+  if (glcontext == NULL) {
     SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
-                    "SDL_ttf font load error: %s\n", SDL_GetError());
+                    "SDL_GL_CreateContext init error: %s\n", SDL_GetError());
     return 1;
   }
-  SDL_Log("SDL_ttf loaded font OK!\n");
-
+  
+  SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &major);
+  SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &minor);
+  SDL_Log("Got an OpenGL %d.%d context\n", major, minor);
+  
   // Load the music
   Mix_Music *music = Mix_LoadMUS("assets/beat.wav");
   if (!music) {
@@ -112,30 +103,19 @@ int main(int argc, char *argv[]) {
   }
   SDL_Log("SDL_mixer loaded sound effect OK!\n");
 
-  // Create the font texture
-  SDL_Rect fontRect;
-  SDL_Color fontColor = {255, 0, 0, 255}; // white
-  SDL_Surface *fontSolid = TTF_RenderText_Solid(font, "AwesomeText", fontColor);
-  SDL_Texture *fontSolidTexture =
-      SDL_CreateTextureFromSurface(renderer, fontSolid);
-  SDL_QueryTexture(fontSolidTexture, NULL, NULL, &fontRect.w, &fontRect.h);
-  fontRect.x = 0;
-  fontRect.y = 0;
-  SDL_Log("SDL_ttf created texture with size %i, %i\n", fontRect.w, fontRect.h);
-
   // Play the music and sound effect
   Mix_PlayChannel(-1, scratch, 0);
   Mix_PlayMusic(music, -1);
 
   // Clear the back buffer
-  SDL_RenderClear(renderer);
+  glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
 
   // Render
-  SDL_RenderCopy(renderer, logoTexture, NULL, &logoRect);
-  SDL_RenderCopy(renderer, fontSolidTexture, NULL, &fontRect);
-
+  // TODO add render code
+  
   // Present the back buffer
-  SDL_RenderPresent(renderer);
+  SDL_GL_SwapWindow(win);
 
   // Wait
   SDL_Delay(2000);
@@ -147,12 +127,7 @@ int main(int argc, char *argv[]) {
   Mix_FreeMusic(music);
   Mix_CloseAudio();
 
-  SDL_DestroyTexture(fontSolidTexture);
-  SDL_FreeSurface(fontSolid);
-
-  SDL_DestroyTexture(logoTexture);
-  SDL_FreeSurface(logoSurface);
-
+  SDL_GL_DeleteContext(glcontext);  
   SDL_Quit();
   return 0;
 }
